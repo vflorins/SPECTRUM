4781a39ab24d596b5f6f6722b229c98e40fe2a43
Juan G Alonso Guzman <33181561+jgaloguz@users.noreply.github.com>
2024-02-09 16:32:56 -0600

    Added example run files
    Added a simple example program for a simulation of GCR modulation in the heliosphere, and a supplementary file to post-process the results.

---
+++ runs/gcr_modulation_example.cc
@@ -0,0 +1,280 @@
+#include "src/simulation.hh"
+#include "src/distribution_other.hh"
+#include "src/background_solarwind.hh"
+#include "src/diffusion_other.hh"
+#include "src/boundary_time.hh"
+#include "src/boundary_space.hh"
+#include "src/initial_space.hh"
+#include "src/initial_momentum.hh"
+#include <iostream>
+#include <iomanip>
+
+using namespace Spectrum;
+
+int main(int argc, char** argv)
+{
+
+   DataContainer container;
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+// Create a simulation object
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+
+   std::unique_ptr<SimulationWorker> simulation;
+   simulation = CreateSimulation(argc, argv);
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+// Particle type
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+
+   int specie = 0;
+   simulation->SetSpecie(specie);
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+// Background
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+
+   container.Clear();
+
+// Origin
+   container.Insert(gv_zeros);
+
+// Velocity
+   double umag = 4.0e7 / unit_velocity_fluid;
+   GeoVector u0(umag, 0.0, 0.0);
+   container.Insert(u0);
+
+// Magnetic field
+   double RS = 6.957e10 / unit_length_fluid;
+   double r_ref = 3.0 * RS;
+   double BmagE = 5.0e-5 / unit_magnetic_fluid;
+   double Bmag_ref = BmagE * Sqr((GSL_CONST_CGSM_ASTRONOMICAL_UNIT / unit_length_fluid) / r_ref);
+   GeoVector B0(Bmag_ref, 0.0, 0.0);
+   container.Insert(B0);
+
+// Effective "mesh" resolution
+   double dmax = GSL_CONST_CGSM_ASTRONOMICAL_UNIT / unit_length_fluid;
+   container.Insert(dmax);
+
+// solar rotation vector
+   double w0 = twopi / (25.0 * 24.0 * 3600.0) / unit_frequency_fluid;
+   GeoVector Omega(0.0, 0.0, w0);
+   container.Insert(Omega);
+
+// Reference equatorial distance
+   container.Insert(r_ref);
+
+// dmax fraction for distances closer to the Sun
+   double dmax_fraction = 0.1;
+   container.Insert(dmax_fraction);
+
+   simulation->AddBackground(BackgroundSolarWind(), container);
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+// Spatial initial condition
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+
+   container.Clear();
+
+   GeoVector init_pos(1.0 * GSL_CONST_CGSM_ASTRONOMICAL_UNIT / unit_length_fluid, 0.0, 0.0);
+   container.Insert(init_pos);
+
+   simulation->AddInitial(InitialSpaceFixed(), container);
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+// Momentum initial condition
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+
+   container.Clear();
+
+// Lower bound for momentum
+   double momentum1 = Mom(10.0 * SPC_CONST_CGSM_MEGA_ELECTRON_VOLT / unit_energy_particle, specie);
+   container.Insert(momentum1);
+
+// Upper bound for momentum
+   double momentum2 = Mom(5000.0 * SPC_CONST_CGSM_MEGA_ELECTRON_VOLT / unit_energy_particle, specie);
+   container.Insert(momentum2);
+
+// Log bias
+   bool log_bias = true;
+   container.Insert(log_bias);
+
+   simulation->AddInitial(InitialMomentumThickShell(), container);
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+// Inner boundary
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+
+   container.Clear();
+
+// Max crossings
+   int max_crossings_Sun = 1;
+   container.Insert(max_crossings_Sun);
+
+// Action
+   std::vector<int> actions_Sun;
+   actions_Sun.push_back(-1);
+   container.Insert(actions_Sun);
+
+// Origin
+   container.Insert(gv_zeros);
+
+// Radius
+   double inner_boundary = 0.05 * GSL_CONST_CGSM_ASTRONOMICAL_UNIT / unit_length_fluid;
+   container.Insert(inner_boundary);
+
+   simulation->AddBoundary(BoundarySphereAbsorb(), container);
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+// Outer boundary
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+
+   container.Clear();
+
+// Max crossings
+   int max_crossings_outer = 1;
+   container.Insert(max_crossings_outer);
+
+// Action
+   std::vector<int> actions_outer;
+   actions_outer.push_back(0);
+   container.Insert(actions_outer);
+
+// Origin
+   container.Insert(gv_zeros);
+
+// Radius
+   double outer_boundary = 80.0 * GSL_CONST_CGSM_ASTRONOMICAL_UNIT / unit_length_fluid;
+   container.Insert(outer_boundary);
+
+   simulation->AddBoundary(BoundarySphereAbsorb(), container);
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+// Time limit
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+
+   container.Clear();
+
+// Not needed because this class sets the value to -1
+   int max_crossings_time = 1;
+   container.Insert(max_crossings_time);
+
+// Action
+   std::vector<int> actions_time;
+   actions_time.push_back(-1);
+   container.Insert(actions_time);
+   
+// Max duration of the trajectory
+   double maxtime = 60.0 * 60.0 * 24.0 * 365.0 / unit_time_fluid;
+   container.Insert(maxtime);
+
+   simulation->AddBoundary(BoundaryTimeExpire(), container);
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+// Diffusion model
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+
+   container.Clear();
+
+// Parallel mean free path
+   double lam0 = 0.1 * GSL_CONST_CGSM_ASTRONOMICAL_UNIT / unit_length_fluid;
+   container.Insert(lam0);
+
+// Rigidity normalization factor
+   double R0 = 1.0e9 / unit_rigidity_particle;
+   container.Insert(R0);
+
+// Magnetic field normalization factor
+   container.Insert(BmagE);
+
+// Power law slope for rigidity
+   double pow_law_R = 0.5;
+   container.Insert(pow_law_R);
+
+// Power law slope for magnetic field
+   double pow_law_B = -1.0;
+   container.Insert(pow_law_B);
+
+// Ratio of kappa_perp to kappa_para
+   double kap_rat = 0.05;
+   container.Insert(kap_rat);
+
+// Pass ownership of "diffusion" to simulation
+   simulation->AddDiffusion(DiffusionRigidityMagneticFieldPowerLaw(), container);
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+// Distribution 1 (spectrum)
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Parameters for distribution
+   container.Clear();
+
+// Number of bins
+   MultiIndex n_bins1(100, 0, 0);
+   container.Insert(n_bins1);
+   
+// Smallest value
+   GeoVector minval1(EnrKin(momentum1), 0.0, 0.0);
+   container.Insert(minval1);
+
+// Largest value
+   GeoVector maxval1(EnrKin(momentum2), 0.0, 0.0);
+   container.Insert(maxval1);
+
+// Linear or logarithmic bins
+   MultiIndex log_bins1(1, 0, 0);
+   container.Insert(log_bins1);
+
+// Add outlying events to the end bins
+   MultiIndex bin_outside1(0, 0, 0);
+   container.Insert(bin_outside1);
+
+// Physical units of the distro variable
+   double unit_distro1 = 1.0 / (Sqr(unit_length_fluid) * unit_time_fluid * fourpi * unit_energy_particle);
+   container.Insert(unit_distro1);
+
+// Physical units of the bin variable
+   GeoVector unit_val1 = {unit_energy_particle, 1.0, 1.0};
+   container.Insert(unit_val1);
+
+// Don't keep records
+   bool keep_records1 = false;
+   container.Insert(keep_records1);
+
+//! Normalization for the "hot" boundary
+   double J0 = 1.0 / unit_distro1;
+   container.Insert(J0);
+
+//! Characteristic energy
+   double T0 = 1.0 * SPC_CONST_CGSM_GIGA_ELECTRON_VOLT / unit_energy_particle;
+   container.Insert(T0);
+
+//! Spectral power law
+   double pow_law_T = -1.8;
+   container.Insert(pow_law_T);
+
+//! Constant value for the "cold" condition
+   double val_cold1 = 0.0;
+   container.Insert(val_cold1);
+
+   simulation->AddDistribution(DistributionSpectrumKineticEnergyPowerLaw(), container);
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+// Run the simulation
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+
+   int n_traj;
+   int batch_size;
+
+   batch_size = n_traj = 1;
+   if(argc > 1) n_traj = atoi(argv[1]);
+   if(argc > 2) batch_size = atoi(argv[2]);
+
+   std::string simulation_files_prefix = "test_artif_and_natur_modulation_parker_analytic_";
+   simulation->DistroFileName(simulation_files_prefix);
+   simulation->SetTasks(n_traj, batch_size);
+   simulation->MainLoop();
+   simulation->PrintDistro1D(0, 0, simulation_files_prefix + "spectrum.dat", true);
+   
+   return 0;
+};
---
+++ runs/postprocess_gcr_modulation_results.cc
@@ -0,0 +1,63 @@
+#include "common/physics.hh"
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace Spectrum;
+
+const double J0 = 1.0;
+const double mu = 1.8;
+const double T0 = SPC_CONST_CGSM_GIGA_ELECTRON_VOLT;
+
+// Unmodulated spectrum
+inline double unmod_spectrum(double T) {return J0 * pow(T / T0, -mu);};
+
+int main(int argc, char** argv)
+{
+   std::string infilename1 = "modulated_gcr_spectrum.dat";
+   std::string outfilename = "modulated_gcr_spectrum_pp.dat";
+   std::string line;
+   int i, N = 100;
+   int sum_c1[N];
+   double energy1[N], distro1[N], sum_w1[N];
+   double scal, v, p2, energy0 = SPC_CONST_CGSM_MEGA_ELECTRON_VOLT;
+
+// Open input distro file
+   std::ifstream input_spectrum_file1(infilename1);
+
+// Read first two lines of distro file
+   std::getline(input_spectrum_file1, line);
+   std::getline(input_spectrum_file1, line);
+
+// Read data
+   for(i = 0; i < N; i++) {
+      input_spectrum_file1 >> energy1[i];
+      input_spectrum_file1 >> distro1[i];
+      input_spectrum_file1 >> sum_w1[i];
+      input_spectrum_file1 >> sum_c1[i];
+   };
+
+// Close input cartesian distro file
+   input_spectrum_file1.close();
+
+// Open output distro file
+   std::ofstream output_spectrum_file(outfilename);
+
+// Output data
+   output_spectrum_file << std::setprecision(8);
+   scal = Vel(Mom(T0 / unit_energy_particle)) * unmod_spectrum(T0);
+   for(i = 0; i < N; i++) {
+      p2 = Sqr(Mom(energy1[i] / unit_energy_particle));
+      v = Vel(Mom(energy1[i] / unit_energy_particle));
+      output_spectrum_file << std::setw(20) << energy1[i] / energy0;
+      output_spectrum_file << std::setw(20) << v * unmod_spectrum(energy1[i]) / scal;
+      output_spectrum_file << std::setw(20) << p2 * distro1[i] / scal;
+      output_spectrum_file << std::endl;
+   };
+
+// Close output distro file
+   output_spectrum_file.close();
+
+   return 0;
+};
