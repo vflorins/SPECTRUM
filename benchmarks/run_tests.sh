cbcc060c2ee2cfd430a1ce236f472d3d49cdca11
Lucius Schoenbaum <luciusschoenbaum@protonmail.com>
2025-05-08 06:00:18 -0500

    updates to run_tests.sh, configure.ac

--- benchmarks/run_tests.sh
+++ benchmarks/run_tests.sh
@@ -12,7 +12,7 @@ short_batch_size=100
 
 # Flags to select which tests to run
 # VISUAL TESTS
-dipole_visual_test=true
+dipole_visual_test=false
 turb_waves_test=false
 parker_spiral_test=false
 init_cond_record_test=false
@@ -42,13 +42,16 @@ function configure {
 
 # Function to make and run test
 function make_and_run {
-	make $1
-	if [ $2 -eq 1 ]
+	make $2
+	mkdir -p $1
+	cd $1
+	if [ $3 -eq 1 ]
 	then
-		[ -d output_data ] || mkdir output_data && cd output_data && ../$1 >> $results_file
+		../$2 >> ../$results_file
 	else
-		[ -d output_data ] || mkdir output_data && cd output_data && mpirun -np $2 ../$1 $3 $4 >> $results_file
-  fi
+		mpirun -np $3 ../$2 $4 $5 >> ../$results_file
+	fi
+	cd -
 }
 
 # Function to return exit code
@@ -68,16 +71,15 @@ function report_if_failed {
 # 1) module load mpi
 # 2) autoreconf
 # 3) automake --add-missing
-mkdir -p "output_data"
 echo -n "Benchmark results " > $results_file
 date >> $results_file
 echo "------------------------------------------------------------" >> $results_file
 
 # DIPOLE FIELD VISUALIZATION
 if $dipole_visual_test
-then	
+then
 	configure SERIAL LORENTZ FORWARD 0 SELF
-	make_and_run main_test_dipole_visualization 1
+	make_and_run output_data main_test_dipole_visualization 1
 fi
 report_if_failed $? "DIPOLE FIELD VISUALIZATION"
 
@@ -85,7 +87,7 @@ report_if_failed $? "DIPOLE FIELD VISUALIZATION"
 if $turb_waves_test
 then
 	configure SERIAL FIELDLINE FORWARD 29 SELF
-	make_and_run main_test_turb_waves 1
+	make_and_run output_data main_test_turb_waves 1
 fi
 report_if_failed $? "TURBULENCE VIA SUPERPOSITION OF WAVES"
 
@@ -93,23 +95,23 @@ report_if_failed $? "TURBULENCE VIA SUPERPOSITION OF WAVES"
 if $parker_spiral_test
 then
 	configure SERIAL FIELDLINE FORWARD 29 SELF
-	make_and_run main_test_parker_spiral 1
+	make_and_run output_data main_test_parker_spiral 1
 fi
 report_if_failed $? "PARKER SPIRAL SOLAR WIND"
 
 # INITIAL CONDITION RECORDS TEST
 if $init_cond_record_test
-then	
+then
 	configure PARALLEL FOCUSED FORWARD 29 SELF
-	make_and_run main_test_init_cond_records $n_cpus $short_sim $short_batch_size
+	make_and_run output_data main_test_init_cond_records $n_cpus $short_sim $short_batch_size
 fi
 report_if_failed $? "INITIAL CONDITION RECORDS TEST"
 
 # DIPOLE FIELD DRIFT PERIODS
 if $dipole_drifts_test
-then	
+then
 	configure SERIAL GUIDING FORWARD 29 SELF
-	make_and_run main_test_dipole_periods 1
+	make_and_run output_data main_test_dipole_periods 1
 fi
 report_if_failed $? "DIPOLE FIELD DRIFT PERIODS"
 
@@ -117,7 +119,7 @@ report_if_failed $? "DIPOLE FIELD DRIFT PERIODS"
 if $pa_distro_iso_test
 then
 	configure PARALLEL GUIDING_SCATT FORWARD 29 SELF
-	make_and_run main_test_pa_distro_isotrop $n_cpus $long_sim $long_batch_size
+	make_and_run output_data main_test_pa_distro_isotrop $n_cpus $long_sim $long_batch_size
 fi
 report_if_failed $? "PITCH ANGLE DISTRIBUTION ISOTROPIZATION"
 
@@ -125,7 +127,7 @@ report_if_failed $? "PITCH ANGLE DISTRIBUTION ISOTROPIZATION"
 if $pa_scatt_test
 then
 	configure PARALLEL GUIDING_SCATT FORWARD 29 SELF
-	make_and_run main_test_pa_scatt $n_cpus $short_sim $short_batch_size
+	make_and_run output_data main_test_pa_scatt $n_cpus $short_sim $short_batch_size
 fi
 report_if_failed $? "PITCH ANGLE SCATTERING"
 
@@ -133,15 +135,15 @@ report_if_failed $? "PITCH ANGLE SCATTERING"
 if $perp_diff_test
 then
 	configure PARALLEL GUIDING_DIFF FORWARD 29 SELF
-	make_and_run main_test_perp_diff $n_cpus $long_sim $long_batch_size
+	make_and_run output_data main_test_perp_diff $n_cpus $long_sim $long_batch_size
 fi
 report_if_failed $? "PERPENDICULAR DIFFUSION"
 
 # FULL (PERP+PARA) DIFFUSION
 if $full_diff_test
-then	
+then
 	configure PARALLEL PARKER FORWARD 29 SELF
-	make_and_run main_test_full_diff $n_cpus $long_sim $long_batch_size
+	make_and_run output_data main_test_full_diff $n_cpus $long_sim $long_batch_size
 fi
 report_if_failed $? "FULL (PERP+PARA) DIFFUSION"
 
@@ -149,10 +151,10 @@ report_if_failed $? "FULL (PERP+PARA) DIFFUSION"
 if $modulation_cartesian_parker_spiral
 then
 	configure PARALLEL PARKER BACKWARD 25 CARTESIAN 1 0
-	make_and_run main_generate_cartesian_solarwind_background 1
+	make_and_run cartesian_backgrounds main_generate_cartesian_solarwind_background 1
 	report_if_failed $? "CARTESIAN PARKER SPIRAL BACKGROUND GENERATION"
-	make_and_run main_test_modulation_cartesian_parker $n_cpus $long_sim $long_batch_size
+	make_and_run output_data main_test_modulation_cartesian_parker $n_cpus $long_sim $long_batch_size
 	report_if_failed $? "MODULATION CARTESIAN PARKER SPIRAL"
-	make_and_run main_postprocess_modulation_cartesian_parker 1
+	make_and_run output_data main_postprocess_modulation_cartesian_parker 1
 	report_if_failed $? "POST-PROCESSING MODULATION CARTESIAN PARKER SPIRAL"
 fi
