b7438fde2d04e2b1c0fbe60cef66120e853b2916
Lucius Schoenbaum <22637852+LuciusSchoenbaum@users.noreply.github.com>
2025-08-29 10:45:44 -0500

    minor changes, mainly to header guards (#66)
    * minor changes, mainly to header guards
    
    * bug fixes and minor changes to SmoothDiscontinuity and SmoothShock
    
    * typo in comments of TrajectoryFieldline
    
    ---------
    
    Co-authored-by: Lucius Schoenbaum <luciusschoenbaum@protonmail.com>

--- src/background_solarwind.hh
+++ src/background_solarwind.hh
@@ -7,8 +7,8 @@
 This file is part of the SPECTRUM suite of scientific numerical simulation codes. SPECTRUM stands for Space Plasma and Energetic Charged particle TRansport on Unstructured Meshes. The code simulates plasma or neutral particle flows using MHD equations on a grid, transport of cosmic rays using stochastic or grid based methods. The "unstructured" part refers to the use of a geodesic mesh providing a uniform coverage of the surface of a sphere.
 */
 
-#ifndef _BACKGROUND_SOLARWIND_HH
-#define _BACKGROUND_SOLARWIND_HH
+#ifndef SPECTRUM_BACKGROUND_SOLARWIND_HH
+#define SPECTRUM_BACKGROUND_SOLARWIND_HH
 
 #include "background_base.hh"
 
