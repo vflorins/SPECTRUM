46034a040fe80922f1086dc1c0912d5b0d593c63
Vladimir Florinski <vaf0001@uah.edu>
2026-06-29 14:38:55 -0500

    Fixed user interface for the two geodesic tests

--- tests/main_test_drawable_sector.cc
+++ tests/main_test_drawable_sector.cc
@@ -1,26 +1,67 @@
 // g++ -I.. -Wall -Wno-comment -std=c++20 -DGEO_DEBUG -g -O0 -o main_test_drawable_sector main_test_drawable_sector.cc ../geodesic/drawable_sector.cc ../geodesic/geodesic_sector.cc ../common/vectors.cc
-// ./main_test_drawable_sector <vert_per_face> <face_division> <sect_division> <ghost_width>
+// ./main_test_drawable_sector -v <vert_per_face> -d <div_dif> -g <ghost_width>
 
-#include <common/definitions.hh>
+#include <config.h>
+
+#ifndef USE_CXXOPTS
+#error This software requires cxxopts library to build
+#endif
+
+#include <cxxopts.hpp>
+#include <common/print_warn.hh>
 #include <geodesic/drawable_sector.hh>
 
 using namespace Spectrum;
 
+/*!
+\brief parse the command-line parameters 
+\author Vladimir Florinski
+\date 06/29/2026
+\param[in]  argc        Number of command-line arguments plus one
+\param[in]  argv        Command-line arguments
+\param[out] vert_face   Vertices per face (3 or 4)
+\param[out] div_dif     Difference between face and sector divisions
+\param[out] ghost_width Width of ghost zone halo
+\return True on success, false if some of the parameters were bad 
+*/
+bool ParseCLOpts(int argc, char** argv, int& vert_face, int& div_dif, int& ghost_width)
+{
+// Declare the command line options
+   cxxopts::Options options("main_test_drawable_sector", "Visualize element numbering and print neighbor information within a sector");
+   options.add_options()("v,vert_face", "Vertices per face (3 or 4)", cxxopts::value<int>()->default_value("3"))
+                        ("d,div_dif", "Difference between face and sector divisions", cxxopts::value<int>()->default_value("3"))
+                        ("g,ghost_width", "Width of ghost zone halo", cxxopts::value<int>()->default_value("1"));
+
+   auto result = options.parse(argc, argv);
+
+   vert_face = result["vert_face"].as<int>();
+   div_dif = result["div_dif"].as<int>();
+   ghost_width = result["ghost_width"].as<int>();
+
+   if ((vert_face < 3) || (vert_face > 4)) {
+      std::cerr << "The \"vert_face\" argument must be either 3 or 4\n";
+      return false;
+   };
+   if (div_dif < 1) {
+      std::cerr << "\"div_dif\" must be greater than 1\n";
+      return false;
+   };
+
+   int width = Pow2(div_dif);
+   if (width < min_width_to_ghost * ghost_width) {
+      std::cerr << "\"ghost_width\" is too large\n";
+      return false;
+   };
+   return true;
+};
+
 int main(int argc, char** argv)
 {
-   int vert_per_face;
-   int face_division = 4;
-   int sect_division = 1;
-   int ghost_width = 3;
+   int vert_per_face, div_dif, ghost_width;
+   if (!ParseCLOpts(argc, argv, vert_per_face, div_dif, ghost_width)) return 1;
 
-   if (argc > 1) vert_per_face = atoi(argv[1]);
-   if ((vert_per_face < 3) || (vert_per_face > 4)) vert_per_face = 3;
-   if (argc > 2) face_division = atoi(argv[2]);
-   if (argc > 3) sect_division = atoi(argv[3]);
-   if (argc > 4) ghost_width = atoi(argv[4]);
-
-   DrawableSector<3> sector3(Pow2(face_division - sect_division), ghost_width);
-   DrawableSector<4> sector4(Pow2(face_division - sect_division), ghost_width);
+   DrawableSector<3> sector3(Pow2(div_dif), ghost_width);
+   DrawableSector<4> sector4(Pow2(div_dif), ghost_width);
 
    for (auto type = 1; type <= 3; type++) {
       std::cerr << std::endl;
--- tests/main_test_drawable_tesselation.cc
+++ tests/main_test_drawable_tesselation.cc
@@ -1,21 +1,60 @@
 // g++ -I.. -Wall -std=c++20 -DGEO_DEBUG -DUSE_SILO -g -O0 -o main_test_drawable_tesselation main_test_drawable_tesselation.cc ../geodesic/spherical_tesselation.cc ../geodesic/drawable_tesselation.cc ../common/vectors.cc
-// ./main_test_drawable_tesselation <conn_division> <draw_division>
+// ./main_test_drawable_tesselation -c <conn_division> -d <draw_division>
 
+#include <config.h>
+
+#ifndef USE_CXXOPTS
+#error This software requires cxxopts library to build
+#endif
+
+#include <cxxopts.hpp>
 #include <common/print_warn.hh>
 #include <geodesic/drawable_tesselation.hh>
 
 using namespace Spectrum;
 
 // Edit these to your needs
-const PolyType poly_type = PolyType::POLY_HEXAHEDRON;
+const PolyType poly_type = PolyType::POLY_ICOSAHEDRON;
 const int max_division = 6;
+
+/*!
+\brief parse the command-line parameters 
+\author Vladimir Florinski
+\date 06/26/2026
+\param[in]  argc     Number of command-line arguments plus one
+\param[in]  argv     Command-line arguments
+\param[out] conn_div Division for which connectivity will be printed
+\param[out] draw_div Division for which images will be generated
+\return True on success, false if some of the parameters were bad 
+*/
+bool ParseCLOpts(int argc, char** argv, int& conn_div, int& draw_div)
+{
+// Declare the command line options
+   cxxopts::Options options("main_test_drawable_tesselation", "Test the tesselation class and print diagnostic information");
+   options.add_options()("c,conn_div", "Division for which connectivity will be printed", cxxopts::value<int>()->default_value("0"))
+                        ("d,draw_div", "Division for which images will be generated", cxxopts::value<int>()->default_value("0"));
+
+   auto result = options.parse(argc, argv);
+
+   conn_div = result["conn_div"].as<int>();
+   draw_div = result["draw_div"].as<int>();
+
+   if ((conn_div < 0) || (conn_div > max_division)) {
+      std::cerr << "The \"conn_div\" argument must be between 0 and " << max_division << std::endl;
+      return false;
+   };
+   if ((draw_div < 0) || (draw_div > max_division)) {
+      std::cerr << "The \"draw_div\" argument must be between 0 and " << max_division << std::endl;
+      return false;
+   };
+
+   return true;
+};
 
 int main(int argc, char** argv)
 {
-   int conn_division = 0;
-   int draw_division = 3;
-   if (argc > 1) conn_division = atoi(argv[1]);
-   if (argc > 2) draw_division = atoi(argv[2]);
+   int conn_division, draw_division;
+   if (!ParseCLOpts(argc, argv, conn_division, draw_division)) return 1;
 
    DrawableTesselation<poly_type, max_division> tesselation;
    std::cerr << "Testing the tesselation class for type " << inf_color << tesselation.GetType() << std_color << " base solid\n";
